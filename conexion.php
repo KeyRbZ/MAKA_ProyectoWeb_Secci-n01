fde42138d9c934867bf2013d0aa2ffef344feb5c
AndreaYeseniaTurciosAparicio <00042524@uca.edu.sv>
2025-10-19 11:33:38 -0600

    feat: levantando el backend

---
+++ conexion.php
@@ -0,0 +1,21 @@
+<?php
+// Configuración para MariaDB (puerto 3307)
+$host = "localhost:3307";
+$user = "root";
+$pass = "";
+$dbname = "maka_proyectoweb";
+
+// Intentar conexión
+try {
+    $conexion = new mysqli($host, $user, $pass, $dbname);
+    
+    if ($conexion->connect_error) {
+        throw new Exception("Error de conexión: " . $conexion->connect_error);
+    }
+    
+    echo "¡Conexión exitosa a MariaDB en puerto 3307!";
+    
+} catch (Exception $e) {
+    die("Error: " . $e->getMessage());
+}
+?>
